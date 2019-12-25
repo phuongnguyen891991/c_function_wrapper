2ae32a65d8a63beb25c89656c971574192e7a44e
phuongnguyen891991 <phuongnguyen891991@gmail.com>
2019-12-25 14:09:48 +0700

    Details: create new function to create thread

--- c_api_func_call.c
+++ c_api_func_call.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <pthread.h>
+#include <ctype.h>
+#include <unistd.h>
 
+#define RET_OK  1
+#define RET_ERR 0
+
+#define MULTIAP_MAC_ADDRESS_STR_LEN 0x0D
+
+#define PRINT_D(x, ...) fprintf( stdout, "%s:%s:%d: " x "\n", __FILE__, __func__, __LINE__, ##__VA_ARGS__)
+
+/*func: this function use to verify mac address
+* Input:
+*      mac_string: is the input mac address
+* Output:
+*       return: RET_OK: if it is successfull
+*               RET_ERR: If it is failed.
+*/
 uint8_t _validateMacString(const char *mac_string)
 {
     uint8_t i;
@@ -9,4 +27,68 @@ uint8_t _validateMacString(const char *mac_string)
             return RET_ERR;
     }
     return RET_OK;
+}
+
+/*Function: to create new thread and check thread is already existed?
+* Input:
+*   thread_id: input parameter for tracking thread
+* output:
+*   RET_OK: Create new thread successfully
+*   RET_ERR: Failed to create thread.
+*/
+
+uint8_t is_existed_thread = 0;
+
+void _helloFunction(void *p)
+{
+    while (1)
+    {
+        PRINT_D("enter hello world \n");
+        sleep(1);
+    }
+}
+
+uint8_t _createNewThread(pthread_t *thread_id)
+{
+    uint8_t ret;
+    uint8_t status;
+
+    ret = RET_ERR;
+    PRINT_D(" Start \n");
+
+    if (is_existed_thread)
+    {
+        PRINT_D("Already have existed thread \n");
+        return ret;
+    }
+
+    status = pthread_create(thread_id, NULL, (void*) _helloFunction, NULL);
+    if (0 != status)
+    {
+        PRINT_D("Failed to create thread \n");
+        return RET_ERR;
+    }
+
+    is_existed_thread = 1;
+    status = pthread_detach(*thread_id);
+    if (0 != status)
+    {
+        PRINT_D("Failed to detach thread \n");
+        return RET_ERR;
+    }
+
+    return RET_OK;
+}
+
+void main()
+{
+    uint8_t status;
+    pthread_t thread1;
+
+    status = _createNewThread(&thread1);
+    if (RET_OK != status)
+    {
+        PRINT_D("Failed to create new thread \n");
+        return;
+    }
 }
